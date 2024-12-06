58c41e3e2af52e12bc58e696b373e50579446a97
Ahmed Mostafa Sayed <eng.ahmeda19@gmail.com>
2024-12-06 14:40:53 +0200

    added the documentation to the operation file.

--- ImageOperation.py
+++ ImageOperation.py
@@ -13,15 +13,30 @@ class ImageOperation:
 			self.cpy_image = copy(self.gray_image)
 
 	def addImage(self) -> np.ndarray:
+		"""
+		Add 2 images together.
+		:return: the result of addition of 2 images.
+		:rtype: np.ndarray
+		"""
 		added_image: np.ndarray = self.gray_image + self.cpy_image
 		added_image = np.clip(added_image, 0, 255)
 		return added_image
 
 	def subtractImage(self) -> np.ndarray:
+		"""
+		subtract 2 images.
+		:return: the result of subtraction of 2 images.
+		:rtype: np.ndarray
+		"""
 		subtracted_image: np.ndarray = self.gray_image - self.cpy_image
 		subtracted_image = np.clip(subtracted_image, 0, 255)
 		return subtracted_image
 
 	def invertImage(self) -> np.ndarray:
+		"""
+		Invert The image.
+		:return: the result of inverting an image.
+		:rtype: np.ndarray
+		"""
 		inverted_image: np.ndarray = 255 - self.gray_image
 		return inverted_image
