acd7bdac9598d1f99edfdcc184ae259446f41588
hossamAhmedSalah <100955137+hossamAhmedSalah@users.noreply.github.com>
2024-12-09 19:46:50 +0200

    omar ColorManipulator class is integrated into the codebase and ready to be used in the GUI

--- test.py
+++ test.py
@@ -3,6 +3,7 @@ import matplotlib.pyplot as plt
 import matplotlib.image as mplt
 # from BasicEdgeDetection import BasicEdgeDetection
 from Filtering import Filtering
+from ColorManipulator import ColorManipulator
 # import cv2
 
 
@@ -21,13 +22,17 @@ from Filtering import Filtering
 # plt.axis("off")
 # plt.show()
 
-image = mplt.imread("images/bad_light_1.jpg")
-filtering = Filtering(image=image)
-low_img, high_img = filtering.applyLowPassFilter(), filtering.applyHighPassFilter()
-plt.subplot(1, 2, 1)
-plt.imshow(low_img, cmap="gray")
-plt.subplot(1, 2, 2)
-plt.imshow(high_img, cmap="gray")
+# image = mplt.imread("images/bad_light_1.jpg")
+image = mplt.imread("images/woody.png")
+color = ColorManipulator(image=image)
+out_image = color.apply_color_filter(1, 3, 1)
+
+# filtering = Filtering(image=image)
+# low_img, high_img = filtering.applyLowPassFilter(), filtering.applyHighPassFilter()
+# plt.subplot(1, 2, 1)
+# plt.imshow(low_img, cmap="gray")
+# plt.subplot(1, 2, 2)
+# plt.imshow(high_img, cmap="gray")
 
 # image_flib = image[:, ::-1]
 # image_flib = image[::-1, :]
@@ -36,11 +41,11 @@ plt.imshow(high_img, cmap="gray")
 
 
 
-# plt.subplot(1, 2, 1)
-# plt.axis("off")
-# plt.imshow(image)
-# plt.subplot(1, 2, 2)
-# plt.imshow(image_flib)
+plt.subplot(1, 2, 1)
+plt.axis("off")
+plt.imshow(image)
+plt.subplot(1, 2, 2)
+plt.imshow(out_image)
 
-# plt.axis("off")
-# plt.show()
+plt.axis("off")
+plt.show()
